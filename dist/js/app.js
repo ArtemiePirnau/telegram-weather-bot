7c0a61a331e01216614cb70285a8ebb338842fc4
ArtemiePirnau <artemieworks@gmail.com>
2022-02-02 15:35:02 +0200

    Add scss folder for gulp

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,80 +1,68 @@
-"use strict"
-//Get elements
-const key = "563492ad6f917000010000016a80b9c70d15412ba67a3c00072cd368";
-const galleryOfPhotos = document.querySelector(".photos");
-const form = document.querySelector(".form");
-const more = document.querySelector(".more__btn");
-let searchInput = document.querySelector(".form__input");
-let searchValue;
-let numberOfPages = 1;
-let presentSearch;
-let getLink;
-
-//Functions
-async function loadPhotos() {
-    numberOfPages++;
-    if (presentSearch) {
-        getLink = `https://api.pexels.com/v1/search?query=${presentSearch}+query&per_page=${numberOfPages}`;
-    } else {
-        getLink = `https://api.pexels.com/v1/curated?page=1&per_page=${page}`;
-    }
-    const data = await getApi(getLink);
-    generateNew(data);
-}
-async function curatedPhotos() {
-    getLink = "https://api.pexels.com/v1/curated?page=1&per_page=10";
-    const data = await getApi(getLink);
-    generateNew(data);
-}
-async function getApi(url) {
-    const getData = await fetch(url, {
-        method: "GET",
-        headers: {
-            Accept: "application/json",
-            Authorization: key,
-        },
-    });
-    const data = await getData.json();
-    return data;
-}
-
-async function searchPhotos(query) {
-    empty();
-    getLink = `https://api.pexels.com/v1/search?query=${query}+query&per_page=10`;
-    const data = await getApi(getLink);
-    generateNew(data);
-}
-
-function empty() {
-    galleryOfPhotos.innerHTML = "";
-    searchInput.value = "";
-}
-
-function generateNew(data) {
-    data.photos.forEach((photo) => {
-        const galleryBlock = document.createElement("div");
-        galleryBlock.classList.add("gallery__image");
-        galleryBlock.innerHTML = `
-    <img src=${photo.src.large}>
-    <div class="gallery__info">
-      <p class="gallery__info-name">${photo.photographer}</p>
-      <a class="gallery__info-link" href=${photo.src.original} target="_blank">Original size</a>
-      <a class="gallery__info-link" href=${photo.src.large} target="_blank">Large size</a>
-    </div>
-    `;
-        galleryOfPhotos.appendChild(galleryBlock);
-    });
-}
-
-function renovateValue(e) {
-    searchValue = e.target.value;
-}
-
-//Event Listeners
-searchInput.addEventListener("input", renovateValue);
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    presentSearch = searchValue;
-    searchPhotos(searchValue);
+const TelegramBot = require('node-telegram-bot-api');
+const request = require('request');
+//telegram bot token
+const token = "5288859798:AAFLg2ujQNztcfjOIpKnO-FNegZMpG_mCKo";
+//weather API token
+const weatherToken = "7c4ab7e231e1b8bafa102cc18b4c86f9"
+    //create a bot
+const bot = new TelegramBot(token, {
+    polling: true
 });
-more.addEventListener("click", loadPhotos);
+
+bot.onText(/\/weather (.+)/, function(msg, match) {
+
+    let city = match[1];
+    let chatId = msg.chat.id
+    let chatText = msg.text;
+    let query = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherToken}`;
+
+    request(query, function(error, response, result) {
+
+        if (!error && response.statusCode == 200) {
+
+            response = JSON.parse(result)
+            let temp = Math.floor((parseInt(
+                response.main.temp_min) - 273.15), 2);
+
+            let pressure = Math.floor(parseInt(
+                response.main.pressure))
+
+            let windSpeed = response.wind.speed;
+
+            let hours = new Date().getHours();
+            let minutes = new Date().getMinutes();
+            let seconds = new Date().getSeconds();
+
+            let currentTime = `${hours}:${minutes}:${seconds}`;
+
+            bot.sendMessage(chatId,
+                `Time in ${response.name} is :`);
+
+            bot.sendMessage(chatId, 'Place: ' +
+                response.name + " 🗺 " + '\nTemperature: ' +
+                String(temp) + '°C ⛅ \nHumidity: ' +
+                response.main.humidity + ' % 🌊 \nWeather: ' +
+                response.weather[0].description + "⚡" +
+                '\nPressure: ' + String(pressure) + " ☂️" +
+                '\nWind Speed: ' + windSpeed + " 🌬 " +
+                '\nCurrent Time: ' + currentTime + "⌚ " +
+                '\nCountry: ' + response.sys.country + " 🌎");
+        }
+    })
+});
+
+bot.onText(/\/start/, function(msg) {
+    const msgId = msg.chat.id;
+    const msgText = msg.text;
+    if (msgText !== "/start") {
+        bot.sendMessage(msgId, "I don't know that command... try another");
+    } else bot.sendMessage(msgId, `Are you using this bot for the first time? Want to know the time in your hometown?  \n \n<b>The commands you can use: </b> \n \n <a href="#">/start - work begins</a> \n <a href="#">/weather - you can see the weather in your city / country / village</a> \n <a href="#">/creator</a> - the GitHub account of the creator of this bot \n \n More features will be coming to this bot soon!`, { parse_mode: "HTML" });
+});
+
+bot.onText(/\/creator/, function(msg) {
+    const msgId = msg.chat.id;
+    const msgText = msg.text;
+    if (msgText !== "/creator") {
+        bot.sendMessage(msgId, "I don't know that command... try another");
+    } else bot.sendMessage(msgId, `Follow me on GitHub https://github.com/ArtemiePirnau`, { parse_mode: "HTML" });
+});
